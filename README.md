9cb449ac0fbeb7cf7293ecb075c268a4ea622770
watsonix <watsonix@gmail.com>
2016-12-20 19:20:57 -0800

    vibration and JSON push stuff

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# HRV-BLE-Cordova
+BLE Heart Rate Demo
 
-Cross platform HRV calcuation with physiological and subjective data logging to server
-
-Connects to a peripherial providing the BLE [Heart Rate Service](http://goo.gl/wKH3X7).
+Connects to a peripherial providing the [Heart Rate Service](http://goo.gl/wKH3X7).
 
 Works with iOS or Android 4.3+.
 
     $ cordova platform add android
     $ cordova plugin add cordova-plugin-ble-central
+    $ cordova plugin add cordova-plugin-vibration
     $ cordova run
--- www/js/index.js
+++ www/js/index.js
@@ -15,11 +15,16 @@
 /* global ble, statusDiv, beatsPerMinute */
 /* jshint browser: true , devel: true*/
 
+//misc settings
+const RRI_MAX = 20;
+
 // See BLE heart rate service http://goo.gl/wKH3X7
 var heartRate = {
     service: '180d',
     measurement: '2a37'
 };
+
+var rrIntervals = [];
 
 var app = {
     initialize: function() {
@@ -27,15 +32,15 @@ var app = {
     },
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
         app.scan();
     },
-    scan: function() {
-        app.status("Scanning for Heart Rate Monitor");
-
+    scan: function(scanTry = 0, extraText = "") {
         var foundHeartRateMonitor = false;
+
+        app.status(extraText + " Scanning for Heart Rate Monitor. Try number " + scanTry);
+        ble.scan([heartRate.service], 5, onScan, scanFailure);
 
         function onScan(peripheral) {
             // this is demo code, assume there is only one heart rate monitor
@@ -49,11 +54,10 @@ var app = {
             alert("BLE Scan Failed");
         }
 
-        ble.scan([heartRate.service], 5, onScan, scanFailure);
-
         setTimeout(function() {
             if (!foundHeartRateMonitor) {
                 app.status("Did not find a heart rate monitor.");
+                app.scan(++scanTry,"Did not find a heart rate monitor.")
             }
         }, 5000);
     },
@@ -65,12 +69,22 @@ var app = {
         alert("Disconnected " + reason);
         beatsPerMinute.innerHTML = "...";
         app.status("Disconnected");
+        setTimeout(app.scan,3000)
     },
     onData: function(buffer) {
-        // assuming heart rate measurement is Uint8 format, real code should check the flags
-        // See the characteristic specs http://goo.gl/N7S5ZS
-        var data = new Uint8Array(buffer);
-        beatsPerMinute.innerHTML = data[1];
+        // var data = new Uint8Array(buffer);
+        measurement = app.parseHeartRateMeasurement(buffer) 
+        beatsPerMinute.innerHTML = measurement.heartRate
+
+        //HRV calculation
+        rrIntervals.push.apply(rrIntervals, measurement.rrIntervals) //add to RRI array
+        while (rrIntervals.length > RRI_MAX) { rrIntervals.shift() } //remove old elements from RRI array
+        std = app.standardDeviation(rrIntervals) 
+        hrvSDRR.innerHTML = std
+        //**** last DEV: 
+        if (std > 100) { 
+            navigator.vibrate(3000); 
+        } //see https://github.com/katzer/cordova-plugin-local-notifications for next level
     },
     onError: function(reason) {
         alert("There was an error " + reason);
@@ -78,7 +92,40 @@ var app = {
     status: function(message) {
         console.log(message);
         statusDiv.innerHTML = message;
-    }
+    },
+    parseHeartRateMeasurement: function (value) {         // See the characteristic specs http://goo.gl/N7S5ZS
+        value = value.buffer ? value : new DataView(value) // check if DataView already. if not convert it
+        let flags = value.getUint8(0)
+        let rate16Bits = flags & 0x1
+        let result = {}
+        let index = 1
+        if (rate16Bits) {
+        result.heartRate = value.getUint16(index, /* littleEndian= */true)
+        index += 2
+        } else {
+        result.heartRate = value.getUint8(index)
+        index += 1
+        }
+        let contactDetected = flags & 0x2
+        let contactSensorPresent = flags & 0x4
+        if (contactSensorPresent) {
+        result.contactDetected = !!contactDetected
+        }
+        let energyPresent = flags & 0x8
+        if (energyPresent) {
+        result.energyExpended = value.getUint16(index, /* littleEndian= */true)
+        index += 2
+        }
+        let rrIntervalPresent = flags & 0x10
+        if (rrIntervalPresent) {
+        let rrIntervals = []
+        for (; index + 1 < value.byteLength; index += 2) {
+          rrIntervals.push(value.getUint16(index, /* littleEndian= */true))
+        }
+        result.rrIntervals = rrIntervals
+        }
+        return result
+    },
     standardDeviation: function(data) {
         const avg = average(data)
         const squareDiffs = data.map(function (value) {
@@ -87,7 +134,15 @@ var app = {
         })
         const avgSquareDiff = average(squareDiffs)
         return Math.sqrt(avgSquareDiff)
-    }
+    },
 };
+
+function average (data) {
+  var sum = data.reduce(function (sum, value) {
+    return sum + value
+  }, 0)
+  return sum / data.length
+};
+
 
 app.initialize();
