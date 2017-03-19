052a5ecd768a36d2817b2569cc4fe0f16e0a99ba
Logan Suguitan <suguloga@Logans-MacBook-Pro.local>
2017-03-19 11:03:58 -0700

    Post to AWS server 4

--- www/js/index.js
+++ www/js/index.js
@@ -20,7 +20,6 @@ var rrIntervals = [];
 const INTERVAL = 30;
 function serverPostHeart (timestamp,values){
 //send to server data on latest RRIs
-    console.log(values)
     if (typeof timestamp === "undefined") { //no args, run with default value (for debugging)
         timestamp = currentTimeISOString()
         payload = {
@@ -30,15 +29,14 @@ function serverPostHeart (timestamp,values){
         }
         serverPost("rr_intervals",payload)
     } else { //package up values in correct format (see https://github.com/danielfennelly/SAAT-API/wiki)
-        for (i = 0; i + 1 < values.length; i += 1) {
-            nextItem = {
-                mobile_time: timestamp,
-                batch_index: i, // Collecting measurements returns a batch of several RRIntervals, and this is the index within a single batch.
-                value: values, // RRI in msec
-            }
-            // payload.push(nextItem) //TODO: eventually package up and post all together
-            serverPost("rr_intervals",nextItem)
+        nextItem = {
+            mobile_time: timestamp,
+            batch_index: 0, // Collecting measurements returns a batch of several RRIntervals, and this is the index within a single batch.
+            value: values, // RRI in msec
         }
+        // payload.push(nextItem) //TODO: eventually package up and post all together
+        console.log(values)
+        serverPost("rr_intervals",nextItem);
     }
 
 };
